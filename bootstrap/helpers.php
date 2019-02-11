61d33d7bd018cbe8970d0dcc4c9f5414542e9a96
shuaishuai.chen <css852438363@163.com>
2019-02-11 10:25:43 +0800

    分类话题列表

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,17 @@
 function route_class(){
 	return str_replace('.', '-', Route::currentRouteName());
 }
+
+/**
+ * Get the active class if the condition is not falsy
+ *
+ * @param        $condition
+ * @param string $activeClass
+ * @param string $inactiveClass
+ *
+ * @return string
+ */
+function active_class($condition, $activeClass = 'active', $inactiveClass = '')
+{
+	
+}
--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -22,11 +22,11 @@
 
                     <div class="media-body meta">
 
-                        <a href="#" title="{{ $topic->category->name }}">
+                        <a href="{{ route('categories.show', $topic->category->id) }}" title="{{ $topic->category->name }}">
                             <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
                              {{ $topic->category->name }}
                         </a>
-
+                        
                         <span> • </span>
                         <a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
                             <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
