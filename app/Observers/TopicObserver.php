b4633b89f9e2cb3fdcd778f7c0b8c5132477cc97
shuaishuai.chen <css852438363@163.com>
2019-02-12 20:48:18 +0800

    用户可以删除自己的回复

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -40,6 +40,11 @@ class TopicObserver
         }
     }
 
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
+
 
 
     
