4df530fdc061a0049ecd86c0f66fa2c517072d2a
shuaishuai.chen <css852438363@163.com>
2019-04-03 18:42:16 +0800

    修正footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,7 +2,9 @@
     <div class="container">
         <p class="pull-left">
             由 <a href="https://github.com/chenshuai1993" target="_blank">陈帅</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            Copyright © 2017-2019 <a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备17042044号</a>
         </p>
+
 
         <p class="pull-right"><a href="/topics/8">关于我</a></p>
     </div>
