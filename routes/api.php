545d3ea4a2e91204de869f90377fa3c91a523abd
shuaishuai.chen <sschend@isoftstone.com>
2019-04-18 15:49:17 +0800

    fix(api): rate limit(接口限流)
    rate limit(接口限流)

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,37 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',[
+$api->version('v1',
+    [
     'namespace' => 'App\Http\Controllers\Api',
-], function($api) {
-    $api->get('version', function() {
-        return response('this is version v11');
-    });
+    ],
+    function($api) {
 
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
+        $api->group(
+            [
+                'middleware' => 'api.throttle',
+                'limit' => config('api.rate_limits.sign.limit'),
+                'expires' => config('api.rate_limits.sign.expires'),
+            ],
+            function($api){
+
+                $api->get('version', function() {
+                    return response('this is version v11');
+                });
+
+                // 短信验证码
+                $api->post('verificationCodes', 'VerificationCodesController@store')
+                ->name('api.verificationCodes.store');
 
 
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
-});
+                // 用户注册
+                $api->post('users', 'UsersController@store')
+                ->name('api.users.store');
+            });
+
+    }
+ );
+
 
 $api->version('v2', function($api) {
     $api->get('version', function() {
