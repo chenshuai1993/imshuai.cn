3a150b4dc111148d1530b30a0ce03d5e0abdfd25
shuaishuai.chen <sschend@isoftstone.com>
2019-04-23 20:46:55 +0800

    fix(api): jwt-token(自动生成token)
    jwt-token(自动生成token)

---
+++ app/Console/Commands/GenerateToken.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class GenerateToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'imshuai:generate-token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '根据用户id生成jew-token';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $userId = $this->ask('输入用户 id');
+
+        $user = User::find($userId);
+
+        if (!$user) {
+            return $this->error('用户不存在');
+        }
+
+        // 一年以后过期
+        $ttl = 365*24*60;
+        $this->info(\Auth::guard('api')->setTTL($ttl)->fromUser($user));
+    }
+}
