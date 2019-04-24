aa02ecbef2d6ca23a62e0cf2741b5fb550ac3b53
shuaishuai.chen <sschend@isoftstone.com>
2019-04-24 16:19:20 +0800

    fix(api): topic update api(更新话题)
    topic update api(更新话题)

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -18,4 +18,13 @@ class TopicsController extends Controller
         return $this->response->item($topic, new TopicTransformer())
             ->setStatusCode(201);
     }
+
+    //更新
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+
+        $topic->update($request->all());
+        return $this->response->item($topic, new TopicTransformer());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1',
     [
         'namespace' => 'App\Http\Controllers\Api',
-        'middleware' => 'serializer:array',
+        'middleware' => ['serializer:array', 'bindings'],
     ],
     function($api) {
 
@@ -81,6 +81,10 @@ $api->version('v1',
                     // 发布话题
                     $api->post('topics', 'TopicsController@store')
                         ->name('api.topics.store');
+
+                    //修改帖子
+                    $api->patch('topics/{topic}', 'TopicsController@update')
+                        ->name('api.topics.update');
                 });
 
             });
