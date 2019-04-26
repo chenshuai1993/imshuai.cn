7420b1381ff970b0ad699e565dc0125d6f3fed0b
shuaishuai.chen <sschend@isoftstone.com>
2019-04-26 11:31:01 +0800

    fix(api): social login change to Passport(social login change to Passport)
    social login change to Passport(social login change to Passport)

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -11,10 +11,14 @@ use Zend\Diactoros\Response as Psr7Response;
 use Psr\Http\Message\ServerRequestInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\AuthorizationServer;
+use App\Traits\PassportToken;
 
 class AuthorizationsController extends Controller
 {
-    public function socialStore($type, SocialAuthorizationRequest $request)
+    use PassportToken;
+
+    //jwt
+    /*public function socialStore($type, SocialAuthorizationRequest $request)
     {
         if (!in_array($type, ['weixin'])) {
             return $this->response->errorBadRequest();
@@ -64,6 +68,59 @@ class AuthorizationsController extends Controller
 
         $token = Auth::guard('api')->fromUser($user);
         return $this->respondWithToken($token)->setStatusCode(201);
+    }*/
+
+    //passport
+    public function socialStore($type, SocialAuthorizationRequest $request)
+    {
+        if (!in_array($type, ['weixin'])) {
+            return $this->response->errorBadRequest();
+        }
+
+        $driver = \Socialite::driver($type);
+
+        try {
+            if ($code = $request->code) {
+                $response = $driver->getAccessTokenResponse($code);
+                $token = array_get($response, 'access_token');
+            } else {
+                $token = $request->access_token;
+
+                if ($type == 'weixin') {
+                    $driver->setOpenId($request->openid);
+                }
+            }
+
+            $oauthUser = $driver->userFromToken($token);
+        } catch (\Exception $e) {
+            return $this->response->errorUnauthorized('参数错误，未获取用户信息');
+        }
+
+        switch ($type) {
+            case 'weixin':
+                $unionid = $oauthUser->offsetExists('unionid') ? $oauthUser->offsetGet('unionid') : null;
+
+                if ($unionid) {
+                    $user = User::where('weixin_unionid', $unionid)->first();
+                } else {
+                    $user = User::where('weixin_openid', $oauthUser->getId())->first();
+                }
+
+                // 没有用户，默认创建一个用户
+                if (!$user) {
+                    $user = User::create([
+                        'name' => $oauthUser->getNickname(),
+                        'avatar' => $oauthUser->getAvatar(),
+                        'weixin_openid' => $oauthUser->getId(),
+                        'weixin_unionid' => $unionid,
+                    ]);
+                }
+
+                break;
+        }
+
+        $result = $this->getBearerTokenByUser($user, '1', false);
+        return $this->response->array($result)->setStatusCode(201);
     }
 
 
